04299bf5eb10133f0e23b4218784fbeaae79889a
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-05-18 20:31:06 -0300

    Update ex06.c

--- Lista2/ex06.c
+++ Lista2/ex06.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
 
-int main (){  
+int main()
+{
 
     int *v;
     int tam, par = 0, impar = 0;
 
     printf("Quantos inteiros serão lidos: ");
-    scanf ("%d", &tam);
+    scanf("%d", &tam);
 
-    v = (int*) malloc(tam * sizeof(int));
+    v = (int *)malloc(tam * sizeof(int));
 
-    if(v == NULL){
+    if (v == NULL)
+    {
         printf("Erro ao alocar memoria \n");
         return 1;
     }
 
-    for(int i = 1; i <= tam; i++){
+    for (int i = 1; i <= tam; i++)
+    {
         printf("%do inteiro: ", i);
         scanf("%d", &v[i]);
     }
 
-    for(int i = 1; i <= tam; i++){
-            if (v[i] % 2 == 0)
+    for (int i = 1; i <= tam; i++)
+    {
+        if (v[i] % 2 == 0)
         {
             par = par + 1;
         }
@@ -31,10 +35,9 @@ int main (){
             impar = impar + 1;
         }
     }
-    
-        printf("Sao pares: %d dos %d inteiros lidos.\n",par, tam);
-        printf("Sao impares: %d dos %d inteiros lidos.",impar, tam);
-    
-    return 0;      
-}
 
+    printf("Sao pares: %d dos %d inteiros lidos.\n", par, tam);
+    printf("Sao impares: %d dos %d inteiros lidos.", impar, tam);
+
+    return 0;
+}
