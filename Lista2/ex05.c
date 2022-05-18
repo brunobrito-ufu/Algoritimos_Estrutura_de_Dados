2a1c7dd6a426ad1a7ac89521e27b4facae8c97a6
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-05-18 14:33:53 -0300

    Update ex05.c

--- Lista2/ex05.c
+++ Lista2/ex05.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>  
-
-int main (){  
+#include <stdlib.h>
 
 
+int main(){
 
+    int *v;
+    int tam = 5;
 
-return 0;      
+    v = (int*) malloc(tam * sizeof(int));
+
+    if(v == NULL){
+        printf("Erro ao alocar memoria \n");
+        return 1;
+    }
+
+    printf("Escreva os valores que serao alocados na memoria:\n");
+
+    for(int i = 0; i < tam; i++){
+        printf("v[%d]: ", i);
+        scanf("%d", &v[i]);
+    }
+
+    for(int i = 0; i < tam; i++){
+        printf("v[%d] = %d  ", i, *(v+i));
+    }
+
+    free(v);
+
+    return 0;
 }
