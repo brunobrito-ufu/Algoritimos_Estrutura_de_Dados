df7984e63768b9d4a30681d0d0f6f684746e5e6d
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-05-20 10:42:12 -0300

    Create ex12.c

---
+++ Lista2/ex12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>  
+
+int main (){  
+
+/*Alocação dinâmica em funções: sabemos que as variáveis locais de uma função (e também
+seus parâmetros) são alocadas na memória no momento da execução da função. Ao término
+da função, essas variáveis são destruídas da memória. Entretanto, quando fazemos uma
+alocação dinâmica dentro de uma função, o vetor alocado permanece na memória, mesmo
+após o término da função.*/
+
+
+return 0;      
+}
+
+
