88dc6633d1eecda1ec79f9fda4355b7ab36f8b5a
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-05-20 10:16:08 -0300

    Update ex09.c

--- Lista2/ex09.c
+++ Lista2/ex09.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
 
-int main (){  
+void calculo(int *x, int *y, int CONSTANTE){
+    int x1, y1;   
+    y1 = *y * CONSTANTE;
+    x1 = *x * CONSTANTE;
+    printf("Resultado: (%d,%d) * %d = (%d,%d)\n", *x, *y, CONSTANTE, x1, y1);
+}
 
+int main(){
 
+    const int CONSTANTE;
+    int x, y;
 
+    printf("Digite o ponto -> (x,y): ");
+    scanf("%d,%d", &x, &y);
 
-return 0;      
+    printf("Digite a constante: ");
+    scanf("%d", &CONSTANTE);
+
+    calculo(&x, &y, CONSTANTE);
+
+  //  printf("%d", x);
+    
+    return 0;
 }
