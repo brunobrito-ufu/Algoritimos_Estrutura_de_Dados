55102230fd37a892490be4c2bdf3eb40fa207b03
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-06-05 20:47:44 -0300

    Update main.c

--- Lista4/main.c
+++ Lista4/main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <"ponto.h">
+
+struct Ponto{
+    float x, y;
+};
 
 
-
-
-
-
-
-
-
+Ponto* gera_pto(float x, float y){
+    Ponto* p = (Ponto*) malloc (sizeof(Ponto))
+    if (p != NULL){
+        p->x = x;
+        p->y = y; 
+    }
+    return p;    
+}
 
 int main()
 {
+
 
 
 
