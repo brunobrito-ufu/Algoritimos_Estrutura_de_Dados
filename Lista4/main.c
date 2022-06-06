8646d9b20417213f743a0914e3664b709db170a9
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-06-06 19:39:05 -0300

    Update main.c

--- Lista4/main.c
+++ Lista4/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ponto.h"
 
+int main()
+{ 
+    float d, x1, x2, y1, y2;
+    Ponto *p, *q;
+
+    printf("Escreva os valores de x1 e x2: ");
+    scanf("%f %f", &x1, &x2);
+    printf("Escreva os valores de y1 e y2: ");
+    scanf("%f %f", &y1, &y2);
+    p = gera_pto(x1,x2);
+    q = gera_pto(y1,y2);
+    d = distancia_pto(p,q);
+
+    printf("A distancia entre os pontos eh de: %.2f\n",d);
+ 
+    libera_pto(p);
+    libera_pto(q);
+
+   return 0;
+}
