2627601309cd63e7a7cf6e7ee408395c0a87b179
brunobrito-ufu <80076693+brunobrito-ufu@users.noreply.github.com>
2022-06-14 18:25:18 -0300

    Update main.c

--- Lista05/main.c
+++ Lista05/main.c
@@ -4,7 +4,29 @@
 #include "listaNO.h"
 
 int main()
-{ 
+{
+    Lista lista;    //a variavel "lista" eh um endereco
+
+    lista = cria_lista();   //cria_lista retorna um endereco
+
+    insere_elem(lista, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    return 0;
 }
